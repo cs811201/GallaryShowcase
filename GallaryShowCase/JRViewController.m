70aed715878466b0d58eec82a64a0982316f5995
孙琳 <sunlin@sunlins-MacBook-Air.local>
2014-08-08 13:48:51 -0700

    Initial Commit

---
+++ GallaryShowCase/JRViewController.m
@@ -0,0 +1,29 @@
+//
+//  JRViewController.m
+//  GallaryShowCase
+//
+//  Created by 孙琳 on 8/8/14.
+//  Copyright (c) 2014 Lin Spirit Tech. All rights reserved.
+//
+
+#import "JRViewController.h"
+
+@interface JRViewController ()
+
+@end
+
+@implementation JRViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ GallaryShowCaseTests/GallaryShowCaseTests.m
@@ -0,0 +1,34 @@
+//
+//  GallaryShowCaseTests.m
+//  GallaryShowCaseTests
+//
+//  Created by 孙琳 on 8/8/14.
+//  Copyright (c) 2014 Lin Spirit Tech. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GallaryShowCaseTests : XCTestCase
+
+@end
+
+@implementation GallaryShowCaseTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
